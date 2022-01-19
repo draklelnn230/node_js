dd3a0b74091a9e102c4cd23e0723a90acaa76717
Lan <h.baolan20020@gmail.com>
2022-01-20 02:14:48 +0700

    Use bootstrap and static file & SCSS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const handlebars  = require('express-handlebars');
+const path = require('path');
 const morgan = require('morgan')
+const hbs = handlebars.create({ extname: '.hbs' })
 const app = express();
 const port = 3000
 
+// Static file & SCSS
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+// Template engine(handlebars)
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, 'resources', 'views'));
+
+// HTTP logger
+app.use(morgan('combined'))
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.render('home');
+})
+
+app.get('/news', (req, res) => {
+    res.render('news')
 })
 
 app.listen(port, () => {
